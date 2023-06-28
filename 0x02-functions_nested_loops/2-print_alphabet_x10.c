84f6c675afdededbaeca3ac54e1094df85b45c78
belmokao <imbchahin@hotmail.fr>
2023-06-28 17:16:29 +0100

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,23 @@
 #include "main.h"
 
+/*
+* print_alphabet_x10 - rpint al
+* return - noting
+*/
 void print_alphabet_x10(void)
 {
+	char a[] = "abcdefghijklmnopqrstuvwxyz\n";
 	int t  = 0;
+	int i;
+
 	while (t < 10)
 	{
-		print_alphabet();
+		i = 0;
+		while (a[i] != '\0')
+		{
+			_putchar(a[i]);
+			i++;
+		}
 		t++;
 	}
 }
