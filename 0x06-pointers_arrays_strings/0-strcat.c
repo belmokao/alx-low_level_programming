779d45e62ffb9f17732332de9b12c813eabff9ab
belmokao <imbchahin@hotmail.fr>
2023-06-29 14:54:03 +0000

    test

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,7 +9,7 @@ char *_strcat(char *dest, char *src)
     int dest_len = strlen(dest);
     int i = 0;
 
-    while (i < n && src[i] != '\0')
+    while (src[i] != '\0')
     {
         dest[dest_len + i] = src[i];
         i++;
