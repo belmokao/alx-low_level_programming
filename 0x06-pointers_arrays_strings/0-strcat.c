e412d066694d7ca6911a51ca0f21912c47fe4fc4
belmokao <imbchahin@hotmail.fr>
2023-06-29 14:50:31 +0000

    test

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+/*
+ * _strcat - take two arguement dest and src
+ *
+ * Return - concatetinated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+    int dest_len = strlen(dest);
+    int i = 0;
+
+    while (i < n && src[i] != '\0')
+    {
+        dest[dest_len + i] = src[i];
+        i++;
+    }
+    dest[dest_len + i] = '\0';
+
+    return dest;
+}
