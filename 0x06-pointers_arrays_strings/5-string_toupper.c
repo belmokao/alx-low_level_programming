2b707b2398d22be1f7e861100beecbde2afd7091
belmokao <imbchahin@hotmail.fr>
2023-06-30 11:53:54 -0700

    test

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,7 +10,7 @@ char *string_toupper(char *str)
 {
 	while (str[i] != '\0')
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+		if (str[i] >= 97 && str[i] <= 122)
 			str[i] = str[i] - 32;
 	}
 	return (str);
