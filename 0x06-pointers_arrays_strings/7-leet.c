4e2772ca801ae2d3ac00d75f0fd8d698563b6df4
belmokao <imbchahin@hotmail.fr>
2023-06-30 21:19:33 +0000

    1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -15,13 +15,13 @@ char *leet(char *str)
     {
         if (str[i] == 'a' || str[i] == 'A')
             str[i] = 4;
-        else if (str[i] == 'e' || str[i] == 'E')
+        if (str[i] == 'e' || str[i] == 'E')
             str[i] = 3;
-        else if (str[i] == 'o' || str[i] == 'O')
+        if (str[i] == 'o' || str[i] == 'O')
             str[i] = 0;
-        else if (str[i] == 't' || str[i] == 'T')
+        if (str[i] == 't' || str[i] == 'T')
             str[i] = 7;
-        else if (str[i] == 'l' || str[i] == 'L')
+        if (str[i] == 'l' || str[i] == 'L')
             str[i] = 1;
         i++;
     }
