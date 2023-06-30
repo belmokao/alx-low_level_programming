595f2358d1a697d255e6b2710ccab527a70555c3
belmokao <imbchahin@hotmail.fr>
2023-06-30 15:01:48 +0000

    new stack

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,21 +1,28 @@
 #include "main.h"
 
 /**
-* _strncpy - copype
-* @dest - destination string
-* @src - src string
-* @n - number of character to copy in
-*/
+ * _strncpy - Copies a string.
+ * @dest: Destination string.
+ * @src: Source string.
+ * @n: Number of characters to copy.
+ *
+ * Return: Pointer to destination string.
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
     int i = 0;
 
-    while(i < n && src[i] != '\0')
+    while (i < n && src[i] != '\0')
     {
         dest[i] = src[i];
         i++;
     }
 
-    return (dest);
+    while (i < n)
+    {
+        dest[i] = '\0';
+        i++;
+    }
 
+    return dest;
 }
