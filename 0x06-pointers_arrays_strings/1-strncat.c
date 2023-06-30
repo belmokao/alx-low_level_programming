b33c74f0b43dc70ba3af78a049c2a203fc960fdb
belmokao <imbchahin@hotmail.fr>
2023-06-30 09:30:18 -0700

    test

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,21 +10,21 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-    int length = 0;
-    int j = 0;
+	int length = 0;
+	int j = 0;
 
-    while (dest[length] != '\0')
-    {
-        length++;
-    }
+	while (dest[length] != '\0')
+	{
+		length++;
+	}
 
-    while (j < n && src[j] != '\0')
-    {
-        dest[length + j] = src[j];
-        j++;
-    }
+	while (j < n && src[j] != '\0')
+	{
+		dest[length + j] = src[j];
+		j++;
+	}
 
-    dest[length + j] = '\0';
+	dest[length + j] = '\0';
 
-    return dest;
+	return (dest);
 }
