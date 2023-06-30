5aef2edda66e2f1796b110ecde858c9060d1cd52
belmokao <imbchahin@hotmail.fr>
2023-06-30 11:36:21 -0700

    update

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * string_toupper - method upercase chars
+ * @str: argurment to
+ *
+ * Return: return upper case string
+ */
+char *string_toupper(char *str)
+{
+	while (str[i] != '\n')
+	{
+		if (str[i] >= 'a' && str[i] < 'z')
+			str[i] = str[i] - 32;
+	}
+	return (str);
+}
