c8970811447a995023c11cb278cd835818290eb4
belmokao <imbchahin@hotmail.fr>
2023-06-30 16:01:48 -0700

    test

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,12 +10,15 @@ char *rot13(char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (s[i++] != '\0')
 	{
-		if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
-			s[i] += 13;
-		else
-			s[i] -= 13;
+		if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+		{
+			if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
+				s[i] += 13;
+			else if ((s[i] >= 'n' && s[i] <= 'z') || (s[i] >= 'N' && s[i] <= 'Z'))
+				s[i] += 13;
+		}
 	}
 
 	return (s);
