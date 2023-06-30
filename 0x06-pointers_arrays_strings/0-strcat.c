71d985a1a388119b821feb25beeb0d3a5b224aa7
belmokao <imbchahin@hotmail.fr>
2023-06-30 09:22:37 -0700

    test 101

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,21 +9,21 @@
  */
 char *_strcat(char *dest, char *src)
 {
-  int length = 0;
-  int j = 0;
-
-  while (dest[length] != '\0')
-    {
-      length++;
-    }
-
-  while (src[j] != '\0')
-    {
-      dest[length + j] = src[j];
-      j++;
-    }
-
-  dest[length + j] = '\0';
-
-  return (dest);
+	int length = 0;
+	int j = 0;
+	
+	while (dest[length] != '\0')
+	{
+		length++;
+	}
+	
+	while (src[j] != '\0')
+	{
+		dest[length + j] = src[j];
+		j++;
+	}
+	
+	dest[length + j] = '\0';
+	
+	return (dest);
 }
