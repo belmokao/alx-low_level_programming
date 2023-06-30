7e2f571620f850517b786a17b4672b153ce1ba49
belmokao <imbchahin@hotmail.fr>
2023-06-30 15:15:32 +0000

    new stack

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,21 +1,25 @@
 #include "main.h"
 
 /**
-* _strcmp - compare two string
-* @s1 : first string to campare to s2
-* @s2 : second string to copare to s1
-*
-* Return - 0 if not fit each other or 1 if good
-*/
+ * _strcmp - Compares two strings.
+ * @s1: First string.
+ * @s2: Second string.
+ *
+ * Return:
+ *   Negative value if s1 < s2.
+ *   Positive value if s1 > s2.
+ *   0 if s1 == s2.
+ */
 int _strcmp(char *s1, char *s2)
 {
     int i = 0;
 
-    while(s1[i] && s2[i])
+    while (s1[i] == s2[i])
     {
-        if (s1[i] != s2[i])
-            return (0);
+        if (s1[i] == '\0')
+            return 0;
         i++;
     }
-    return (1);
+
+    return s1[i] - s2[i];
 }
