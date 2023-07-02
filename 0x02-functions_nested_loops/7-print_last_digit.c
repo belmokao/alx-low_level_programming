1179b62bd0dcb2586603a03faad30444b646e77d
belmokao <imbchahin@hotmail.fr>
2023-07-02 11:20:24 +0100

    test

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - do required task
+ * @a: input
+ * 
+ *Return: ouput desired
+*/
+int print_last_digit(int a)
+{
+    int b = a % 10;
+
+    return b;
+}
