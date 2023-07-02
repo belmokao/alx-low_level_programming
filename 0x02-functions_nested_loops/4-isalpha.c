140dc90e2eca800c81545920740dfaa8ab75cbc5
belmokao <imbchahin@hotmail.fr>
2023-07-02 10:59:10 +0100

    test

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,3 +1,11 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for alpha
+ * @c: char input
+ * 
+ * Return: 0 or 1 for each case
+*/
 int _isalpha(int c)
 {
 	if ((c > 97 && c <= 122) || (c >= 65 && c <= 90))
