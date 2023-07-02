804e2e277df635af58d2dbb14ae40566e6d339d1
belmokao <imbchahin@hotmail.fr>
2023-07-02 02:52:03 -0700

    ttest

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,16 +4,12 @@
 * _islower - chekc chars is lowwer
 * @c : input char
 *
-* Return - 0 or 1 in return 
+* Return: 0 or 1 in return
 */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
