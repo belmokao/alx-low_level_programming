18d94ee5080d109c5811f749370c22ac2c25de96
belmokao <imbchahin@hotmail.fr>
2023-07-02 09:55:41 -0700

    test

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,38 +7,38 @@
  */
 void times_table(void)
 {
-    int i, c, j;
+	int i, c, j;
 
-    i = 0;
-    c = 0;
-    while (i < 10)
-    {
-        j = 0;
-        c = 0;
-        while (j < 10)
-        {
-            if (c < 10)
-            {
-                if (j != 0)
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                    _putchar(' ');
-                }
-                _putchar(c + '0');
-            }
-            else if (c >= 10)
-            {
-                _putchar(',');
-                _putchar(' ');
-                _putchar((c / 10) + '0');
-                _putchar((c % 10) + '0');
-            }
-            c = c + i;
-            j++;
-        }
-        _putchar('\n');
-        i++;
-        c++;
-    }
+	i = 0;
+	c = 0;
+	while (i < 10)
+	{
+		j = 0;
+		c = 0;
+		while (j < 10)
+		{
+			if (c < 10)
+			{
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(c + '0');
+			}
+			else if (c >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}
+			c = c + i;
+			j++;
+		}
+		_putchar('\n');
+		i++;
+		c++;
+	}
 }
