b8236b7582b553ce5885a478d9069facadfd3962
belmokao <imbchahin@hotmail.fr>
2023-07-02 10:08:21 -0700

    update

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer
- * starting from 00:00 to 23:5
- * */
+* jack_bauer - time print 
+* start from midnight to midnight - 1minite
+*/
 void jack_bauer(void)
 {
 	int i, j;
-	
+
 	i = 0;
 	while (i < 24)
 	{
