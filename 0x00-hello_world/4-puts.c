c7c2d32de3c6f08c789d0c92a45be8b295fbc1df
belmokao <imbchahin@hotmail.fr>
2023-09-11 06:18:18 -0700

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	char *link = "Programming is like building a multilingual puzzle\n";
+	char *link = "\"Programming is like building a multilingual puzzle\n";
 
 	while (*link != '\0')
 		write(1, link++, 1);
