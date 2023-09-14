1410c08aa5604809c5d67862e83d06a2520f5d03
belmokao <imbchahin@hotmail.fr>
2023-09-14 14:43:37 -0700

    message

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-* _isupper - method check for uppercase chars
-* @void: no input
-* Retrun: return is int 0 or 1
+*print_numbers - method check for uppercase chars
+*@void: no input
+*Return: return is int 0 or 1
 */
 
 void print_numbers(void)
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*print_most_numbers - prints numbers except 2 and 4
+*
+*Return: returns nothing
+*/
+void print_most_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i % 2 == 0 && i <= 9)
+		_putchar('0' + i);
+}
+
