861b5a8aee14b7aa7cc5642aa53a1fc1cfd2ee1f
belmokao <imbchahin@hotmail.fr>
2023-09-14 14:35:14 -0700

    updage commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 /**
-* _isdigit - method check for uppercase chars
-* @c: is char input
-* Retrun: return is int 0 or 1
+*_isdigit - method check for uppercase chars
+*@c: is char input
+*Return: return is int 0 or 1
 */
 
 int _isdigit(int c)
--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,8 +1,8 @@
 /**
-* mul - method check for uppercase chars
-* @a: is int input
-* @b: is int input
-* Retrun: return is multi
+*mul - method check for uppercase chars
+*@a: is int input
+*@b: is int input
+*Return: return is multi
 */
 
 int mul(int a, int b)
