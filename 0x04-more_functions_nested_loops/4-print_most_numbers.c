357207fe81665203cb8a569a300f044ac6ed1cec
belmokao <imbchahin@hotmail.fr>
2023-09-14 15:19:17 -0700

    updated

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,8 +10,13 @@ void print_most_numbers(void)
 	int i;
 
 	i = 0;
-	while (i++ <= 9)
-		if (i != 2 || i != 4)
+	while (i <= 9)
+	{
+		if ((i != 2) || (i != 4))
+		{
 			_putchar('0' + i);
+		}
+		i++;
+	}
 	_putchar('\n');
 }
