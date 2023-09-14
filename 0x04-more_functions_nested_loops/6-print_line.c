16b98391603376d21038634adb45815d94946023
belmokao <imbchahin@hotmail.fr>
2023-09-14 15:54:11 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*print_line - prints numbers
+*@n: is repeat time
+*Return: returns nothing
+*/
+void print_line(int n)
+{
+	while (i < n)
+	{
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
+}
