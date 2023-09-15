1210fbaa084cbf8f8738029058b2461c9130572c
belmokao <imbchahin@hotmail.fr>
2023-09-15 14:59:27 -0700

    tld

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,10 +1,25 @@
-#include <stdio.h>
+#include "stdio.h"
 
 /**
 *main - check the code
 *
 *Return: Always 0
 */
+
+/**
+*isprime - chekc n is prime number
+*
+*Return: 0 or 1 dependent in case
+*/
+int isprime(int n)
+{
+	if (n == 2 || n == 3)
+		return (1);
+	if (n % 2 == 0 || n % 3 == 0)
+		return (0);
+	return (1);
+}
+
 int main(void)
 {
 	int p;
@@ -31,4 +46,3 @@ int main(void)
 
 	return (0);
 }
-
