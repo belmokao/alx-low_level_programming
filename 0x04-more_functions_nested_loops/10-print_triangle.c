9333824a9a8708e856cc745f632e34ada6550297
belmokao <imbchahin@hotmail.fr>
2023-09-15 16:57:29 -0700

    traingle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+*print_triangle - prints a diagonal
+*@n:parameter
+*
+*Return: returns nothing
+*/
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int k;
+
+	if (size <= 0)
+		_putchar('\n');
+
+	i = 1;
+	while (i <= size)
+	{
+		j = 0;
+		while (j++ < size - i)
+			_putchar(' ');
+		k = 0;
+		while (k++ < i)
+			_putchar('#');
+		_putchar('\n');
+		i++;
+	}
+}
