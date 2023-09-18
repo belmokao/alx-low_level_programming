b693ddef23c9f6fd30cc67a0309178396e1b98c7
belmokao <imbchahin@hotmail.fr>
2023-09-18 04:19:12 -0700

    test

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *_atoi - method print chars number
+ *@s: string intput
+ *
+ *Return: return int converter from string
+ */
+int _atoi(char *s)
+{
+	char sign;
+	int n;
+
+	sign = '0'
+	while (s != '\0')
+	{
+		if (s == '-' && sign == '-')
+			sign = '+';
+		else if (s == '+' && sign == '-')
+			sign = '-';
+		else
+			sign = '-';
+		if (s >= '0' && s <= '9')
+		{
+			if (n >= 0)
+				n = (s + '0') + (n * 10);
+			else
+				n = s + '0';
+		}
+	}
+}
