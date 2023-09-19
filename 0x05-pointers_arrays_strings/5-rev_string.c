8960d71f8759310f91e13a5ef251b5ac747981cc
belmokao <imbchahin@hotmail.fr>
2023-09-19 06:45:17 -0700

    test

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,10 +1,26 @@
+#include "main.h"
+
+/**
+*rev_string - rev string
+*@s: input
+*
+*Return: Void
+*/
 void rev_string(char *s)
 {
-	while (*s != '\0')
+	int i;
+	int l;
+
+	l = 0;
+	while (s[l] != '\0')
+		l++;
+	i = 0;
+	while (i < l / 2)
 	{
-		char c = *(s + 1);
-		*(s + 1) = *s;
-		*s = c;
-		s++;
+		char t = s[i];
+
+		s[i] = s[l - 1 - i];
+		s[l - 1 - i] = t;
+		i++;
 	}
 }
