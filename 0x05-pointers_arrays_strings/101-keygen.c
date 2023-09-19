ed1c5b9cec44628e9d5b6b576febced0d2dd24be
belmokao <imbchahin@hotmail.fr>
2023-09-19 10:55:02 -0700

    test

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,4 +1,6 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 /**
  * main - check the code for Holberton School students.
@@ -7,6 +9,20 @@
  */
 int main(void)
 {
+	char c;
+	int x;
 
+	srand(time(0));
+	int l;
+
+	l = (rand() /2) % 128;
+	printf("%d\n", l);
+	//return 0;
+	while (x <= l)
+	{
+		c = (rand() / 2)% 128;
+		putchar(c);
+		x++;
+	}
 	return (0);
 }
