0d23f45ae7903829ee8a1949998305196129e690
belmokao <imbchahin@hotmail.fr>
2023-09-20 16:04:16 -0700

    upd

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,7 +5,7 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
-char *rot13(char *);
+char *rot13(char *s;
 char *cap_string(char *);
 char *leet(char *);
 int _strcmp(char *s1, char *s2);
