630d74f294b60c9416dcfd88f0ad9dcf38d066c1
belmokao <imbchahin@hotmail.fr>
2023-09-20 11:52:33 -0700

    updae

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
@@ -9,3 +12,5 @@ int _strcmp(char *s1, char *s2);
 int _putchar(char a);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+#endif /*MAIN_H*/
