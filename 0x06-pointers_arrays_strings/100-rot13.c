dffced43389b2e69c841a900f5dceb5d2813a90a
belmokao <imbchahin@hotmail.fr>
2023-09-20 16:38:11 -0700

    upds

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -13,11 +13,11 @@ char *rot13(char *s)
 	i = 0;
 	while (s[i] != '\0')
 	{
-		if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+	       while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
 		{
 			if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
 				s[i] += 13;
-			else if ((s[i] >= 'n' && s[i] <= 'z') || (s[i] >= 'N' && s[i] <= 'Z'))
+			else
 				s[i] -= 13;
 		}
 		i++;
