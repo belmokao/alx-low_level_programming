d0c09065440bf894ca040aa38aa2a1101c417eae
belmokao <imbchahin@hotmail.fr>
2023-09-20 15:19:23 -0700

    strv

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,20 +8,21 @@
  */
 char *cap_string(char *str)
 {
-        int i = 0;
-        char dots [] = ",;.!?(){}\n\t\" \0";
-        int j = 0;
+	int i;
+	char dots[] = ",;.!?(){}\n\t\" ";
+	int j;
 
-        while (str[i] != '\0')
-        {
-                while (dots[j] != '\0')
-                {
-                        if (str[i] == dots[j] && (str[i + 1] >= 97 && str[i + 1] <= 122))
-                                str[i + 1] -= 32; 
-                        j++;
-                }
-                j = 0;
-                i++;
-        }
-        return (str);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (dots[j] != '\0')
+		{
+			if (str[i] == dots[j] && (str[i + 1] >= 97 && str[i + 1] <= 122))
+				str[i + 1] -= 32;
+			j++;
+		}
+		i++;
+	}
+	return (str);
 }
