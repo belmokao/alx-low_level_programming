e7ea55f1d7f853ffd458cb13fdbdbdd95f3de378
belmokao <imbchahin@hotmail.fr>
2023-09-20 14:45:48 -0700

    utlknsdf[

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _strcat - Concatenates two strings.
- * @dest: Pointer to destination string.
- * @src: Pointer to source string.
- *
- * Return: Pointer to destination string.
- */
+*_strcat - Concatenates two strings.
+*@dest: Pointer to destination string.
+*@src: Pointer to source string.
+*
+*Return: Pointer to destination string.
+*/
 char *_strcat(char *dest, char *src)
 {
 	int length = 0;
