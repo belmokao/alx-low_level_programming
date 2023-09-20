6e127cb053cd944bc110ef497a96174d0a0a3834
belmokao <imbchahin@hotmail.fr>
2023-09-20 11:49:39 -0700

    updae

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,45 +1,82 @@
 #include "main.h"
-#include <stdio.h>
 
+/**
+*atoii - ato
+*@str: inpupt
+*
+*Return: int
+*/
 int atoii(char *str)
 {
-    int result = 0;
-    int sign = 1;
-    int i = 0;
+	int result = 0;
+	int sign = 1;
+	int i = 0;
 
-    if (str[0] == '-')
-    {
-        sign = -1;
-        i = 1;
-    }
+	if (str[0] == '-')
+	{
+		sign = -1;
+		i++;
+	}
 
-    while (str[i] != '\0')
-    {
-        int digit = str[i] - '0';
-        result = result * 10 + digit;
-        i++;
-    }
-    return result * sign;
+	while (str[i] != '\0')
+	{
+		int digit = str[i] - '0';
+
+		result = result * 10 + digit;
+		i++;
+	}
+	return (result * sign);
 }
 
+/**
+*strlenn - length
+*@s: input
+*
+*Return: length
+*/
 int strlenn(char *s)
 {
-    int i = 0;
+	int i = 0;
 
-    while (*(s + i) != '\0')
-    {
-        i++;
-    }
-    return i;
+	while (*(s + i) != '\0')
+		i++;
+	return (i);
 }
 
+/**
+*infinite_add - infiinte
+*@n1, input 1
+*@n2: input 2
+*@r: riri
+*@size_r: size of buffer
+*
+*Return: buffer
+*/
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    if (strlenn(n1) > size_r || strlenn(n2) > size_r)
-        return (0);
-    int resu = atoii(n1) + atoii(n2);
-    printf("test %d \n", atoii(n2));
-    r = (char *)resu;
-    return r;
-    return (0);
+	if (strlenn(n1) + strlenn(n2) > size_r)
+		return (0);
+
+	int resu = atoii(n1) + atoii(n2);
+	int i = 0;
+
+	while (resu > 0)
+	{
+		r[i] = '0' + resu % 10;
+		resu /= 10;
+		i++;
+	}
+
+	int j = 0;
+
+	while (i > j)
+	{
+		char c = r[j];
+
+		r[j] = r[i - 1];
+		r[i - 1] = c;
+		j++;
+		i--;
+	}
+	return (r);
 }
