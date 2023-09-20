d09776ac43f1439388deb20757e5565874b4d497
belmokao <imbchahin@hotmail.fr>
2023-09-20 04:52:57 +0100

    Update main.h bik

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,5 +5,12 @@ int _atoi(char *s);
 int _putchar(char);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void puts_half(char *str);
+void _puts(char *str);
+void print_rev(char s);
+void rev_string(char *s);
+void puts2(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 
 #endif /* MAIN_H */
